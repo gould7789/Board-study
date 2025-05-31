179a5c3503a845f38b18e29a44c56a850aebab6d
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-31 19:39:23 +0900

    add comment

--- frontend/insert.php
+++ frontend/insert.php
@@ -3,6 +3,8 @@
 require_once '../backend/board_model.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // 값이 없을 경우 빈 문자열로 초기화
     $name = $_POST['name'] ?? '';
     $password = $_POST['password'] ?? '';
     $subject = $_POST['subject'] ?? '';
@@ -10,10 +12,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // 필수 항목 체크
     if ($name && $password && $subject && $content) {
+
+        // 게시글 삽입 함수
         insertPost($name, $password, $subject, $content);
         header("Location: board_login.php");
         exit;
     } else {
+        // 빈 항목 있을시 오류 메시지 출력
         echo "모든 항목을 입력해주세요.";
     }
 }
