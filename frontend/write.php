05f9f00a1ce46d063b02a18dfa0dc19cf44c1992
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-05-31 19:37:44 +0900

    add comment

--- frontend/write.php
+++ frontend/write.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<!-- 글쓰기 폼 만들기 -->
+<!-- 글쓰기 폼 페이지 -->
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
@@ -8,13 +8,15 @@
 
 <body>
     <h2>게시글 작성</h2>
+
+    <!-- 게시글 작성 폼. insert.php로 전송 -->
     <form method="post" action="insert.php">
-        이름: <input type="text" name="name" required><br><br>
-        비밀번호: <input type="password" name="password" required><br><br>
-        제목: <input type="text" name="subject" required><br><br>
+        이름: <input type="text" name="name" required><br><br>              <!-- 이름 입력 -->
+        비밀번호: <input type="password" name="password" required><br><br>  <!-- 작성자 확인용 비밀번호 입력 -->
+        제목: <input type="text" name="subject" required><br><br>           <!-- 제목 입력 -->
         내용:<br>
-        <textarea name="content" rows="10" cols="50" required></textarea><br><br>
-        <input type="submit" value="작성 완료">
+        <textarea name="content" rows="10" cols="50" required></textarea><br><br>   <!-- 본문 작성 -->
+        <input type="submit" value="작성 완료">     <!-- 작성 완료 버튼 -->
     </form>
 </body>
 </html>
