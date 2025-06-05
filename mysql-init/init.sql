1eb3a50182ce7d3cfdfb01373752d0f5cf6a26f5
Leehyeonwoo <hyeon.woo7789@gmail.com>
2025-06-05 20:18:14 +0900

    comment update

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,15 +1,18 @@
-CREATE DATABASE IF NOT EXISTS board_login
+-- 게시판용 데이터베이스
+-- 데이터베이스 생성 및 중복이 있을시 무시하고 실행
+CREATE DATABASE IF NOT EXISTS board_login   -- 생성할 데이터 베이스 이름 
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 USE board_login;
 
+-- 글 저장 테이블 생성
 CREATE TABLE IF NOT EXISTS board (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR(20) NOT NULL,
-    password CHAR(20) NOT NULL,
-    subject CHAR(20) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- 글 번호
+    name CHAR(20) NOT NULL,             -- 작성자 이름
+    password CHAR(20) NOT NULL,         -- 비밀번호
+    subject CHAR(20) NOT NULL,          -- 제목
+    content TEXT NOT NULL,              -- 내용
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP  -- 작성 시간
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
